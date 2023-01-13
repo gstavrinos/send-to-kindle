7133b3117f4668d63c91e7995497d3540d5b274d
gstavrinos <gstavrinos@protonmail.com>
2023-01-13 01:33:18 +0000

    Deploying to gh-pages from @ gstavrinos/send-to-kindle@d17a75f529dbc41d0446ab69ba91d65a6033249e 🚀

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"send_to_kindle":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.1/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"send_to_kindle/struct.Args.html\" title=\"struct send_to_kindle::Args\">Args</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
