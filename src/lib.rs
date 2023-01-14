e05a02e11d45bd00aa7cd02c7744d2965becc80c
George Stavrinos <gstavrinos@protonmail.com>
2023-01-14 02:17:55 +0200

    Updated documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,42 @@
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/gstavrinos/send-to-kindle/master/media/send-to-kindle256.png", 
+    html_favicon_url = "https://raw.githubusercontent.com/gstavrinos/send-to-kindle/master/media/send-to-kindle128.png",
+    html_root_url = "https://docs.rs/send_to_kindle")
+]
+
+//!
+//! <img src="https://raw.githubusercontent.com/gstavrinos/send-to-kindle/master/media/send-to-kindle.png" style="width: 330px;"/>
+//!
+//! # Send to Kindle
+//!
+//! send-to-kindle is a command-line utility and rust library for sending files to your kindle app
+//! or devices by (ab)using the www.amazon.com./sendtokindle web interface.
+//!
+//! **For this reason, it should be used with caution. Getting suspended by Amazon's spam
+//! prevention systems is always a possibility. USE send-to-kindle AT YOUR OWN RISK!**
+//!
+//! # Command-line tool basic usage
+//!
+//! ```
+//! cargo run -- --username <username> --password <password> --directory <path_to_books>
+//! --extension epub
+//! ```
+//!
+//! The `--directory` flag can be swapped with the `--file` flag to just send a single file. If an
+//! extension is provided, it will ensure that the selected file has the requested extension.
+//! 
+//! For more info on the command-line utility and flags for corner cases, use the `--help` flag.
+//!
+//! # Library usage
+//!
+//! Just two functions are provided: One for a list of strings representing the files to be
+//! uploaded (<a href="fn.send_files_to_kindle.html">send_files_to_kindle</a>), and one for a path to a file or directory that can be filtered using a string
+//! for the files' extension (<a href="fn.send_to_kindle.html">send_to_kindle</a>). (epub, azw3, mobi etc).
+//!
+//! For more info, click on each function's definition and read the extensive documentation there.
+//!
+//!
+
 use thirtyfour::prelude::{WebDriverResult, By};
 use thirtyfour::{FirefoxCapabilities, WebDriver};
 use thirtyfour::common::capabilities::firefox::FirefoxPreferences;
